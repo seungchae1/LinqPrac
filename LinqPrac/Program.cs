6c014ee16953db3dc2c7dc17e6b0b6da739a7601
seungchae <s2125@e-mirim.hs.kr>
2023-08-07 14:13:22 +0900

    Linq를 사용하지 않은 세련되지 않은 방법

--- LinqPrac/Program.cs
+++ LinqPrac/Program.cs
@@ -10,6 +10,19 @@ namespace LinqPrac
     {
         static void Main(string[] args)
         {
+            // Linq를 사용하지 않은 세련되지 않은 방법
+
+            List<int> input = new List<int>() {1,2,3,4,5,6,7,8,9,10};
+            List<int> output = new List<int>();
+
+            foreach (int item in input)
+            {
+                if(item % 2 ==0) output.Add(item);
+            }
+            foreach (var item in output)
+            {
+                Console.WriteLine(item);   
+            }
         }
     }
 }
